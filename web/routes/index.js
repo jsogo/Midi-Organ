da3e1866e692aa3ae9d33afd220db8025a4a6ff8
AngelAlvie <alvareza@mit.edu>
2017-10-26 16:23:09 -0400

    Added web funhctionality

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '205 Directory' });
 });
+
+router.get('/terminal', function(req, res, next) {
+  res.render('terminal', {title: 'Terminal'});
+});
+
+router.get('/organ', function(req, res, next) {
+  res.render('organ', {title: 'Pi Organ'});
+});
+
+router.get('/links', function(req, res, next) {
+  res.render('links', {title:"Awesome Links"});
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: "About Us" });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: "Contact Us" });
+});
+
+
 
 module.exports = router;
