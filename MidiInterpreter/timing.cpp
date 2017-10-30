c8ab73d2d8abc248762278090c169f76ed75ee61
Emma <emmakbat@gmail.com>
2017-10-30 09:26:08 -0500

    timing implemented

--- MidiInterpreter/timing.cpp
+++ MidiInterpreter/timing.cpp
@@ -1,13 +1,24 @@
 #include "timing.h"
+
+#include <wiringPi.h>
+#include <softPwm.h>
 #include <thread>
+#include <chrono>
+#include <iostream>
 
 namespace midi{
 
 
 void Timing::playSong(std::unordered_map<double, std::vector< std::vector<int> >> gpio_data, std::vector<double> time){
+	double prev_time = 0;
 	for(unsigned i = 0; i < time.size(); ++i){
-		//std::this_thread::sleep_for(p_midis[i].delta_t); //duration
-		//gpio lib command p_midis[i].command[0] -- pin# p_midis[i].command[1] --high/low		
+		std::this_thread::sleep_for(std::chrono::duration<double>(time[i]-prev_time));
+		prev_time = time[i];
+		for(unsigned event = 0; event < gpio_data.at(time[i]).size(); event++){
+			int signal = gpio_data.at(time[i])[event][0];
+	        	int note = gpio_data.at(time[i])[event][1];	
+			std::cout << "time " << i << " signal " << signal << " note " << note << std::endl;
+		}		
 	}
 }
 
