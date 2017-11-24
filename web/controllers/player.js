4d8eca1c76ab14fe5cadfa1e74fa1f2792a49715
AngelAlvie <alvareza@mit.edu>
2017-11-24 14:33:28 -0500

    Made web player actually capable of playing, currently in testing.

--- web/controllers/player.js
+++ web/controllers/player.js
@@ -1,89 +1,103 @@
 const settings = require("../settings");
 const fs = require('fs');
-let exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 
-var currentSong = settings.songs['song2'];
-var songQueue = [settings.songs['song1'], settings.songs['song3'], settings.songs['song4'], settings.songs['song2']];
-var currentProcess = null;
+var songQueue = [];
 
-/* I want the executor code to look something like this:
-
-Spawn returns a childObject, which you can then listen for events with. The events are:
-
-Class: ChildProcess
-Event: 'error'
-Event: 'exit'
-Event: 'close'
-Event: 'disconnect'
-Event: 'message'
-There are also a bunch of objects from childObject, they are:
-
-Class: ChildProcess
-child.stdin
-child.stdout
-child.stderr
-child.stdio
-child.pid
-child.connected
-child.kill([signal])
-child.send(message[, sendHandle][, callback])
-child.disconnect()
-
-
-var spawn = require('child_process').spawn;
-var child = spawn('node ./commands/server.js');
-child.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
-    //Here is where the output goes
-});
-child.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
-    //Here is where the error output goes
-});
-child.on('close', function(code) {
-    console.log('closing code: ' + code);
-    //Here you can get the exit code of the script
-});
-
-*/
-
-update = function() {
-  command = settings.scriptPath + currentSong[path] + " &> ./web/current.out";
- 
-}
-
-getNextSong = function() {
-  return songQueue.shift();
-};
-
-module.exports.add = function(songName) {
+var push = function(songName) {
   var song = settings.songs[songName];
   if (song) {
-    if (currentSong) {
-      songQueue.push(song);
-    } else {
-      currentSong = song;
-    }
-    console.log("Added a new song to the queue");
+    songQueue.push(song);
   }
+}
+var pop = function() {
+  return songQueue.shift();
+}
+
+var currentSong = null;
+var currentProcess = null;
+
+var executor = function() {
+  //run command
+  //Attempt to find song 
+    songPath =currentSong.path;
+    command = settings.scriptPath + " " + songPath;
+    command = "/home/alvareza/Desktop/tail"; //testing on angel's pc
+    console.log("Executing: " + command);
+    try {
+      var child = execFile(command, (err, stdout, sterr) => {
+        if (err) {
+          handler(true, false, err);
+        } else if (stderr) {
+          handler(true, false, stderr);     // just print on data
+        } else if (stdout) {
+          handler(true, false, stdout);     // just print on data
+        } else {
+          handler(true, false, "Finished Program");;
+        }
+
+      });
+
+      return child;
+    } catch (err) {
+      handler(true, false, "Could not play " + currentSong.title + ". " + err);
+      return null;
+    }
 };
+//current song will only be put up IFF there is literally a song playing
+var handler = function(skip=false, restart=false, msg='') {
+  if (msg) {
+    console.log("HANDLER: " + msg);
+  }
+  if (skip) {
+    currentSong = null;
+    if (currentProcess) {
+      currentProcess.kill();
+      currentProcess = null;
+    }
+  }
+  if (currentSong) {
+    if (restart) {
+      if (currentProcess) {
+        currentProcess.kill();
+        currentProcess = null;
+      }
+      currentProcess = executor();
+    } 
+  } else {
+    if (songQueue.length > 0) {
+      currentSong = pop();
+      console.log("Now Playing: " + currentSong.title);
+      if (currentProcess) {
+        currentProcess.kill();
+        currentProcess = null;
+      }
+      currentProcess = executor();
+    }
+  }
+}
+
+module.exports.add = function(songName) {
+  push(songName);
+  handler(false, false, "Added a new song to the queue");
+};
+
 
 module.exports.remove = function (songIndex) {
   if (songIndex >= 0 && songIndex < songQueue.length) {
     songQueue.splice(songIndex,1);
-    console.log("Removed a song at " + songIndex);
   }
+  handler(false, false,"Removed a song at " + songIndex)
 };
 
 module.exports.restart = function() {
   //kill current process, and reinitialize the command
-  console.log("Restarted current song.");
+  handler(false, true, "Restart current song.");
 };
 
 module.exports.skip = function() {
   //kill current process and set the current song to the next song
-  currentSong = getNextSong();
-  console.log("Skipped current song.");
+  handler(true, false, "Skipped current song.");
 };
 
 module.exports.queueLength = function() {
